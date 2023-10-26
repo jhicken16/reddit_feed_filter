22b78ad01279a7dc48ab7a539fcca35745155d6d
Jhicken16 <jhicken16@gmail.com>
2023-10-26 10:55:33 +0100

    FeedFilter.js the title and button has been connected using aria for accessiblit. button is some what associated with title on screen add styling for clearer guid.

--- src/utilities/feedFilter/FeedFilter.js
+++ src/utilities/feedFilter/FeedFilter.js
@@ -8,8 +8,8 @@ export default function FeedFilter({title, filterHandler}){
     //turn button into a switch button
     return (
         <div>
-            <h3>{ title }</h3>
-            <button onClick={() => filterHandler(title) }>filter</button>
+            <h3 id={title}>{ title }</h3>
+            <button aria-labelledby={title} onClick={() => filterHandler(title) }>filter</button>
         </div>    
     )
 }
